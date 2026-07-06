c210f07f3df6cc6907c81dac8998eec0e04f4708
sami-ramadan <samiramadan389@gmail.com>
2026-07-06 14:32:01 +0300

    feat: add Create New Design button to Designs component and update localization for new design creation

--- components/account/designs/designs.tsx
+++ components/account/designs/designs.tsx
@@ -1,6 +1,9 @@
 import PageTitle from "../shared/page-title";
 import { getTranslations } from "next-intl/server";
 import DesignCard from "./design-card";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Link } from "@/i18n/navigation";
 
 export default async function Designs() {
   const t = await getTranslations("Account.Designs");
@@ -18,6 +21,18 @@ export default async function Designs() {
         {[...Array(3)].map((_, index) => (
           <DesignCard key={index} />
         ))}
+
+        <div className="col-span-1 sm:col-span-2 md:col-span-3">
+          <Link href="/builder">
+            <Button
+              variant="outline"
+              className="border-2 bg-white cursor-pointer w-70 h-70 border-dashed flex-col"
+            >
+              <Plus className="size-9 text-primary" />
+              {t("CreateNewDesign")}
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
