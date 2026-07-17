f3de209ba996d5eeb05bcb4499860b4f0a664c6d
sami-ramadan <samiramadan389@gmail.com>
2026-07-17 23:27:39 +0300

    feat: update button styling for AddToShopCartBtn component

--- components/shop/add-to-shop-cart-btn.tsx
+++ components/shop/add-to-shop-cart-btn.tsx
@@ -27,7 +27,7 @@ export default function AddToShopCartBtn({
     <Button
       aria-label={`Add ${item.name} to cart`}
       onClick={addToCart}
-      className="h-10 text-base bg-primary hover:bg-secondary cursor-pointer absolute z-20 bottom-2 left-1/2 transform -translate-x-1/2 w-2/3 py-2.5 text-white font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+      className="h-10 text-base bg-primary hover:bg-secondary cursor-pointer absolute z-20 bottom-2 left-1/2 transform -translate-x-1/2 w-2/3 py-2.5 text-white font-semibold lg:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
     >
       <ShoppingCart className="mr-2 inline-block size-5" />
       {t("AddToCart")}
