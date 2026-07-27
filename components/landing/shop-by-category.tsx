41ae933fbf612640066bf853d062167b3ef952c0
sami-ramadan <samiramadan389@gmail.com>
2026-07-27 13:31:45 +0300

    fix: simplify API call in Categories component by removing unnecessary parameters

--- components/landing/shop-by-category.tsx
+++ components/landing/shop-by-category.tsx
@@ -9,14 +9,7 @@ import type { CategoriesResponse } from "@/types/landing";
 import { Link } from "@/i18n/navigation";
 
 async function Categories() {
-  const { data, ok } = await http.get<CategoriesResponse>(
-    "/api/v1/categories",
-    {
-      params: {
-        per_page: 5,
-      },
-    },
-  );
+  const { data, ok } = await http.get<CategoriesResponse>("/api/v1/categories");
 
   if (!ok) {
     throw new Error("Failed to fetch categories");
@@ -24,7 +17,7 @@ async function Categories() {
 
   return (
     <>
-      {data.data.items.map((item, index) => (
+      {data.data.items.slice(0, 5).map((item, index) => (
         <motion.div
           initial={{ opacity: 0, y: 40 }}
           whileInView={{ opacity: 1, y: 0 }}
