15473ca85d728dc4fd033d85e363f48ccf09d429
sami-ramadan <samiramadan389@gmail.com>
2026-08-13 20:43:53 +0300

    feat: add Privacy Policy and Terms & Conditions pages with dynamic content fetching

---
+++ app/[locale]/(main)/terms/page.tsx
@@ -0,0 +1,22 @@
+import { http } from "@/lib/http";
+import { AppSettings } from "@/types/landing";
+
+export default async function Page() {
+  const { data, ok } = await http.get<{
+    data: AppSettings;
+  }>(`/api/v1/settings`);
+
+  if (!ok) {
+    throw new Error("Failed to fetch app settings");
+  }
+
+  return (
+    <main className="container max-w-7xl py-20 min-h-[50vh]">
+      <div
+        dangerouslySetInnerHTML={{
+          __html: data.data.terms_and_conditions || "",
+        }}
+      />
+    </main>
+  );
+}
